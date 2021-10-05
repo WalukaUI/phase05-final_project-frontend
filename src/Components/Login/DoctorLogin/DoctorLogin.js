26c7ee773e526d14ba9c2100b700ed2aaab3e7b9
WalukaUI <cwaluka@yahoo.com>
2021-10-05 01:42:19 -0700

    doc login form

--- src/Components/Login/DoctorLogin/DoctorLogin.js
+++ src/Components/Login/DoctorLogin/DoctorLogin.js
@@ -1,33 +1,52 @@
-import React from "react";
-import "./DocLogin.css"
+import React, { useState } from "react";
+import "./DocLogin.css";
 
-function DoctorLogin(){
-return(<>
-    <div className="sidenav">
-         <div className="login-main-text">
-            <h2>Application<br/> Login Page</h2>
-            <p>Login or register from here to access.</p>
-         </div>
+function DoctorLogin() {
+ const [docEmail,setDocEmail]=useState("")
+ const [password, setPassword] = useState("");
+
+  return (
+    <div className="row docloginMain">
+      <div className="sidenav col col-md-6 col-sm-12">
+        <div className="login-main-text">
+          <h2>
+            Doctor
+            <br /> Login Page
+          </h2>
+          <p>Login or register from here to access.</p>
+        </div>
       </div>
-      <div className="main">
-         <div className="col-md-6 col-sm-12">
-            <div className="login-form">
-               <form>
-                  <div className="form-group">
-                     <label>User Name</label>
-                     <input type="text" className="form-control" placeholder="User Name"/>
-                  </div>
-                  <div className="form-group">
-                     <label>Password</label>
-                     <input type="password" className="form-control" placeholder="Password"/>
-                  </div>
-                  <button type="submit" className="btn btn-black">Login</button>
-                  <button type="submit" className="btn btn-secondary">Register</button>
-               </form>
-            </div>
-         </div>
+      <div className="main col col-md-6 col-sm-12">
+        <form>
+          <div>
+            <label>email</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="email"
+              value={docEmail}
+              onChange={(e) => setDocEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type="submit" className="btn btn-success">
+            Login
+          </button>
+          <button type="submit" className="btn btn-warning">
+            Register
+          </button>
+        </form>
       </div>
-      </>
-)
+    </div>
+  );
 }
-export default DoctorLogin
+export default DoctorLogin;

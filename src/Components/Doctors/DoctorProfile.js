080d32c5d3aa49b23cd14af88b005af21fa8c94c
WalukaUI <cwaluka@yahoo.com>
2021-10-05 13:22:06 -0700

    doctor profile styling

--- src/Components/Doctors/DoctorProfile.js
+++ src/Components/Doctors/DoctorProfile.js
@@ -18,28 +18,44 @@ function DoctorProfile() {
   }, [params.id]);
 
   return <>
-      <div>
+      <div className="profile">
         <div className="row profileTopDiv">
-          <div className="col col-sm-12 col-md-4"></div>
-          <div className="col col-sm-12 col-md-8"></div>
+          <div className="col col-sm-12 col-md-4">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPPYSMvGn6iF_kZQs8YyU1jm4VczEPSx2Lpw&usqp=CAU"
+            alt="doctor"
+          />
+          </div>
+          <div className="col col-sm-12 col-md-4 aboutDoctor">
+            <h5>{docProfile.first_name} {docProfile.last_name}</h5>
+            <p>Speciality: {docProfile.speciality}</p>
+            <div>
+              <h6>Average Ratings</h6>
+               <p>⭐⭐⭐⭐⭐</p>
+               <a href="!#">Comments & Ratings</a>
+            </div>
+          </div>
+          <div className="col col-sm-12 col-md-4 appointmentBtnDiv">
+          <button className="btn profileBtn">Schedule Appointment</button>
+          </div>
         </div>
-        <div className="row proLinksDiv">
-          <div className="col col-sm-12 col-md-4">About</div>
-          <div className="col col-sm-12 col-md-4">Ratings and Comments {docProfile.first_name}</div>
-          <div className="col col-sm-12 col-md-4">Make an Appointment</div>
+        <div className="row profileLinksDiv">
+          <div className="col col-sm-12 col-md-4"><a href="!#">About</a></div>
+          <div className="col col-sm-12 col-md-4"><a href="!#">Ratings and Comments</a></div>
+          <div className="col col-sm-12 col-md-4"><a href="!#">Make an Appointment</a></div>
         </div>
         <div className="row profileAboutDiv">
           <div className="col col-sm-12 col-md-4">Location and Contact Information</div>
           <div className="col col-sm-12 col-md-4">{docProfile.first_name} {docProfile.last_name}, MD</div>
           <div className="col col-sm-12 col-md-4">Make an Appointment</div>
         </div>
-      </div>
-      <h4>Patient Ratings and Comments</h4>
+        <h4>Patient Ratings and Comments</h4>
       <div style={{backgroundColor: "#00586F", height: "100px"}}>Rating</div>
       <div style={{backgroundColor: "#00586F", height: "100px"}}>Comments</div>
       <div className="row profileCommentsDiv">
         <div className="col">L</div>
         <div className="col">she is ggod</div>
+      </div>
       </div>
     </>
 }

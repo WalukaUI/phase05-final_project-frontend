cc3616bb0bc5c9fac91488bf1ace849a3b08c52c
WalukaUI <cwaluka@yahoo.com>
2021-10-05 04:09:47 -0700

    rerouting to doctor profile

--- src/Components/Doctors/DoctorCard.js
+++ src/Components/Doctors/DoctorCard.js
@@ -1,24 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./DoctorCard.css";
 
 function Doctor({ card }) {
-  return <div>
+  return (
+    <div>
+      <Link to={`/doctors/${card.id}`} style={{ textDecoration: 'none' }}>
       <div className="row doctorCrad">
         <div className="col col-sm-12 col-md-4 docImage">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPPYSMvGn6iF_kZQs8YyU1jm4VczEPSx2Lpw&usqp=CAU" alt="doctor"/>
-        </div>
-        <div className="col col-sm-12 col-md-4" >
-          <h6>{card.first_name} {card.last_name}</h6>
-          <p>{card.speciality}</p>
-          
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPPYSMvGn6iF_kZQs8YyU1jm4VczEPSx2Lpw&usqp=CAU"
+            alt="doctor"
+          />
         </div>
         <div className="col col-sm-12 col-md-4">
-        <p>Location: {card.clinic_location}</p>
-        <p><img src="./contact"alt="contact"/>{card.contact_number}</p>
-        </div>  
+          <h5>
+            {card.first_name} {card.last_name}
+          </h5>
+          <p>{card.speciality}</p>
+          <p>Ratings</p>
+        </div>
+        <div className="col col-sm-12 col-md-4">
+          <p>Location: {card.clinic_location}</p>
+          <p>
+            <img src="./contact.svg" alt="contact" />
+            {card.contact_number}
+          </p>
+          <button className="btn cardBtn">Schedule Appointment</button>
+        </div>
       </div>
+      </Link>
     </div>
-
+  );
 }
 
 export default Doctor;

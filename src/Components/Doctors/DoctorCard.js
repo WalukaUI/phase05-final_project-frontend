96caa3d065bcbb859060f6d2e302f24b718a8f89
WalukaUI <cwaluka@yahoo.com>
2021-10-05 03:41:30 -0700

    doctor card styling

--- src/Components/Doctors/DoctorCard.js
+++ src/Components/Doctors/DoctorCard.js
@@ -3,9 +3,19 @@ import "./DoctorCard.css";
 
 function Doctor({ card }) {
   return <div>
-      <div className="row">
-      <h1>{card.email}</h1>
-        <p>{card.last_name}</p>
+      <div className="row doctorCrad">
+        <div className="col col-sm-12 col-md-4 docImage">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPPYSMvGn6iF_kZQs8YyU1jm4VczEPSx2Lpw&usqp=CAU" alt="doctor"/>
+        </div>
+        <div className="col col-sm-12 col-md-4" >
+          <h6>{card.first_name} {card.last_name}</h6>
+          <p>{card.speciality}</p>
+          
+        </div>
+        <div className="col col-sm-12 col-md-4">
+        <p>Location: {card.clinic_location}</p>
+        <p><img src="./contact"alt="contact"/>{card.contact_number}</p>
+        </div>  
       </div>
     </div>
 
--- src/Components/Doctors/Doctors.js
+++ src/Components/Doctors/Doctors.js
@@ -1,64 +1,103 @@
 import React, { useEffect, useState } from "react";
-import BASE_URL from "../../constraints/URL"
+import BASE_URL from "../../constraints/URL";
 import "./Doctors.css";
-import Doctor from "./DoctorCard"
-import CardLoadAnimation from "./DocCardLoading"
+import Doctor from "./DoctorCard";
+import CardLoadAnimation from "./DocCardLoading";
 
 function Doctors() {
-const [doctors, setDoctors]=useState(null)
+  const [doctors, setDoctors] = useState(null);
 
-    useEffect(()=>{
-        fetch(BASE_URL + `/doctors`, {
-            method: "GET",
-            headers: { "Content-Type": "application/json" },
-            credentials: "include",
-          }).then((res) => {
-            console.log(res);
-            if (res.ok) {
-              res.json().then((data) => {
-                // setisloggedin(true);
-                setDoctors(data);
-                console.log(data);
-              });
-            }else{
-                console.log(res);
-            //   setLogin(!login)
-            }
-          });
-    },[])
- //const showDoc=doctors.map((card,index)=> <Doctor key={index} card={card}/>)
-  return <div>
+  useEffect(() => {
+    fetch(BASE_URL + `/doctors`, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+    }).then((res) => {
+      console.log(res);
+      if (res.ok) {
+        res.json().then((data) => {
+          // setisloggedin(true);
+          setDoctors(data);
+        });
+      } else {
+        console.log(res);
+        //   setLogin(!login)
+      }
+    });
+  }, []);
+
+  return (
+    <div>
       <div className="row doctorMainDiv">
         <div className="col col-sm-12 col-md-3 docSearchDiv">
           <form>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Last name"
-              />
-              <button className="btn btn-primary searchBtn">Search</button>
-             <div className="serchFilter">
-                 <h6>Narrow Your Results</h6>
-                <ul>
-                    <li className="serchTearms"><label>Accepting New Patients</label><div><input type="checkbox"/></div></li>
-                    <li className="serchTearms"><label>Video Visits</label><div><input type="checkbox"/></div></li>
-                </ul>
-                <p>Speciality</p>
-                <ul>
-                    <li className="serchTearms"><label>Dermatalogist</label><div><input type="checkbox"/></div></li>
-                    <li className="serchTearms"><label>Family Medicine</label><div><input type="checkbox"/></div></li>
-                    <li className="serchTearms"><label>Pediatric</label><div><input type="checkbox"/></div></li>
-                    <li className="serchTearms"><label>Internal Medicine</label><div><input type="checkbox"/></div></li>
-                    <li className="serchTearms"><label>Gynecology</label><div><input type="checkbox"/></div></li>
-                </ul>
-                
-             </div>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Last name"
+            />
+            <button className="btn btn-primary searchBtn">Search</button>
+            <div className="serchFilter">
+              <h6>Narrow Your Results</h6>
+              <ul>
+                <li className="serchTearms">
+                  <label>Accepting New Patients</label>
+                  <div>
+                    <input type="checkbox" />
+                  </div>
+                </li>
+                <li className="serchTearms">
+                  <label>Video Visits</label>
+                  <div>
+                    <input type="checkbox" />
+                  </div>
+                </li>
+              </ul>
+              <p>Speciality</p>
+              <ul>
+                <li className="serchTearms">
+                  <label>Dermatalogist</label>
+                  <div>
+                    <input type="checkbox" />
+                  </div>
+                </li>
+                <li className="serchTearms">
+                  <label>Family Medicine</label>
+                  <div>
+                    <input type="checkbox" />
+                  </div>
+                </li>
+                <li className="serchTearms">
+                  <label>Pediatric</label>
+                  <div>
+                    <input type="checkbox" />
+                  </div>
+                </li>
+                <li className="serchTearms">
+                  <label>Internal Medicine</label>
+                  <div>
+                    <input type="checkbox" />
+                  </div>
+                </li>
+                <li className="serchTearms">
+                  <label>Gynecology</label>
+                  <div>
+                    <input type="checkbox" />
+                  </div>
+                </li>
+              </ul>
+            </div>
           </form>
         </div>
         <div className="col col-sm-12 col-md-9 formnput">
-            {doctors === null ? <CardLoadAnimation />: doctors.map((card,index)=> <Doctor key={index} card={card}/>)}
+          {doctors === null ? (
+            <CardLoadAnimation />
+          ) : (
+            doctors.map((card, index) => <Doctor key={index} card={card} />)
+          )}
         </div>
       </div>
     </div>
+  );
 }
 export default Doctors;

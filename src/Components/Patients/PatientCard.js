919cbb8adad2826cb988389b7008f1a86f1bced6
WalukaUI <cwaluka@yahoo.com>
2021-10-05 22:27:26 -0700

    patient edit pannel styling

--- src/Components/Patients/PatientCard.js
+++ src/Components/Patients/PatientCard.js
@@ -1,16 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import "./PatientCard.css";
 
-function PatientCard({ card, deletePatient }) {
+function PatientCard({ card, deletePatient, editPatient }) {
+  const [editPanel, setEditPanel] = useState(false);
+
   function handleDelete(e) {
     e.preventDefault();
-    deletePatient(card.id)
+    deletePatient(card.id);
   }
 
-  return (
+  function handelEdit(e) {
+    e.preventDefault();
+    setEditPanel(!editPanel);
+  }
+
+  function triggerEdit(e) {
+    e.preventDefault();
+    editPatient(card.id);
+  }
+
+  return editPanel ? (
+    <div className="personsCard">
+      <form>
+        <div>
+          <label>First Name</label>
+          <input name="first_name" />
+          <label>Last Name</label>
+          <input name="last_name" />
+        </div>
+        <div>
+          <h6>Contact Details</h6>
+          <label>First Name</label>
+          <input name="first_name" />
+          <label>Last Name</label>
+          <input name="last_name" />
+        </div>
+
+        <br />
+        <button className="btn btn-danger" type="submit" onClick={triggerEdit}>
+          Save
+        </button>
+      </form>
+      <button
+        className="btn btn-danger"
+        onClick={() => setEditPanel(!editPanel)}
+      >
+        Cancel
+      </button>
+    </div>
+  ) : (
     <div className="personsCard">
       <div>
-        <button className="btn btn-primary">Edit</button>
+        <button className="btn btn-primary" onClick={handelEdit}>
+          Edit
+        </button>
         <button className="btn btn-info">More...</button>
         <p>First Name: {card.first_name}</p>
         <p>Last Name: {card.last_name}</p>

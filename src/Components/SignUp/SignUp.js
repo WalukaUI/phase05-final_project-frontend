93c6c686f950acbe860fe0d5905c1704c867b28e
WalukaUI <cwaluka@yahoo.com>
2021-10-07 01:06:53 -0700

    form styled

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import "./SignUp.css"
+import "./SignUp.css";
 
-function SignUp({location}) {
+function SignUp({ location }) {
   const [newPatient, setNewPatient] = useState({});
 
   function createNewPatient(e) {
@@ -13,40 +13,42 @@ function SignUp({location}) {
   }
 
   return (
-    <div className="row signupContainer">
+    <div className="signupContainer">
+        <h4>Create your account</h4>
       <form onSubmit={createNewPatient}>
-        <div className="col col-sm-12 col-md-6">
-          <label>
-            First Name
-            <input
-              className="form-control form-control-sm"
-              type="text"
-              name="first_name"
-              placeholder="First Name"
-              onChange={handleAddPatient}
-            />
-          </label>
-          <label>
-            Last Name
-            <input
-              className="form-control form-control-sm"
-              type="text"
-              name="last_name"
-              placeholder="Last Name"
-              onChange={handleAddPatient}
-            />
-          </label>
-          <label>
-            Email
-            <input
-              className="form-control form-control-sm"
-              type="text"
-              name="email"
-              placeholder="Name"
-              onChange={handleAddPatient}
-            />
-          </label>
-          <label>
+        <div className="row signupInnerContainer">
+          <div className="col col-sm-12 col-md-6 signUpformInnerDiv1">
+            <label>
+              First Name
+              <input
+                className="form-control form-control-sm"
+                type="text"
+                name="first_name"
+                placeholder="First Name"
+                onChange={handleAddPatient}
+              />
+            </label>
+            <label>
+              Last Name
+              <input
+                className="form-control form-control-sm"
+                type="text"
+                name="last_name"
+                placeholder="Last Name"
+                onChange={handleAddPatient}
+              />
+            </label>
+            <label>
+              Email
+              <input
+                className="form-control form-control-sm"
+                type="text"
+                name="email"
+                placeholder="Name"
+                onChange={handleAddPatient}
+              />
+            </label>
+            <label>
               City
               <select
                 className="form-select"
@@ -67,42 +69,44 @@ function SignUp({location}) {
                 <option value="11">Hyman</option>
               </select>
             </label>
-        </div>
+          </div>
 
-        <div className="col col-sm-12 col-md-6">
-          <label>
-            Username
-            <input
-              className="form-control form-control-sm"
-              name="username"
-              placeholder="Username"
-              onChange={handleAddPatient}
-            />
-          </label>
-          <label>Password</label>
-          <input
-            type="password"
-            name="password"
-            autocomplete="on"
-            className="form-control"
-            onChange={handleAddPatient}
-            placeholder="Password"
-          />
-          <label>Password Verification</label>
-          <input
-            type="password"
-            name="password_confirmation"
-            autocomplete="on"
-            className="form-control"
-            onChange={handleAddPatient}
-            placeholder="Veryfy your Password"
-          />
-        </div>
-
-        <div className="col-sm">
-          <button className=" btn btn-success createPatientBtn" type="submit">
+          <div className="col col-sm-12 col-md-6 signUpformInnerDiv2">
+            <label>
+              Username
+              <input
+                className="form-control form-control-sm"
+                name="username"
+                placeholder="Username"
+                onChange={handleAddPatient}
+              />
+            </label>
+            <label>
+              Password
+              <input
+                type="password"
+                name="password"
+                autocomplete="on"
+                className="form-control"
+                onChange={handleAddPatient}
+                placeholder="Password"
+              />
+            </label>
+            <label>
+              Password Verification
+              <input
+                type="password"
+                name="password_confirmation"
+                autocomplete="on"
+                className="form-control"
+                onChange={handleAddPatient}
+                placeholder="Veryfy your Password"
+              />
+            </label>
+            <button className=" btn btn-success createPatientBtn" type="submit">
             Submit
           </button>
+          </div>
         </div>
       </form>
     </div>

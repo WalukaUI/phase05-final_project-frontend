baaf09bee8f71a5d8f94ab5960a8777e37661641
WalukaUI <cwaluka@yahoo.com>
2021-10-07 13:17:51 -0700

    heroku url added to constrains/url

--- src/constraints/URL.js
+++ src/constraints/URL.js
@@ -2,6 +2,6 @@
 const BASE_URL=
 process.env.NODE_ENV === "development"
 ?"http://localhost:3000"
-:"https://doctorbookingapp.herokuapp.com"
+:"https://guarded-hamlet-25175.herokuapp.com"
 
 export default BASE_URL

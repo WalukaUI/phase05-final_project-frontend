92ad154eaa13eb6687f25720033cebad25394d85
WalukaUI <cwaluka@yahoo.com>
2021-10-06 20:02:54 -0700

    component structure created

--- src/Components/Locations/Locations.js
+++ src/Components/Locations/Locations.js
@@ -1,8 +1,19 @@
 import react from "react";
 
 function Locations(){
-    return(
-        <p>Locarjsdfas</p>
+    return(<div>
+        <div className="row">
+            <div className="col col-sm-12 col-md-6">
+               <div>
+               <h3>google Map</h3>
+               </div>
+            </div>
+            <div className="col col-sm-12 col-md-6">
+            <p>Locarjsdfas</p>
+            </div>
+        </div>
+    </div>
+        
     )
 }
 

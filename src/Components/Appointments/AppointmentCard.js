f101f505c6c898f2332d8c52de028bc7094124f0
WalukaUI <cwaluka@yahoo.com>
2021-10-08 01:55:55 -0700

    edit appointment fun

--- src/Components/Appointments/AppointmentCard.js
+++ src/Components/Appointments/AppointmentCard.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { useState } from "react/cjs/react.development";
 import "./Appointments.css"
 
-function AppointmentCard({card, deleteAppointment}){
+function AppointmentCard({card, deleteAppointment, editAppointment}){
+    const[updatedAppointment,setUpdatedAppointment]=useState({})
 
     function handleDelete(e){
         e.preventDefault()
         deleteAppointment(card.id)
+    }
+
+    function handleChange(e) {
+        e.preventDefault()
+       
+    }
+
+    function handleEdit(e) {
+        e.preventDefault()
+        editAppointment(card)
     }
 
     return(<div className=" row appointmentCard">
@@ -14,7 +26,7 @@ function AppointmentCard({card, deleteAppointment}){
         <h6>Time: {card.time}</h6>
         </div>
         <div className="col col-md-6 col-sm-12">
-        <button className="btn patientDetlsBtn">Patient Details</button>
+        <button className="btn patientDetlsBtn">Change Appointment</button>
         <button className="btn btn-danger cardDeleteBtn" onClick={handleDelete}>Delete</button>
         </div>
         </div>

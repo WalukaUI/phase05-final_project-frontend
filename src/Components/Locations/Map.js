2f34c482f47999ba9f7340e1afbcfdf763e7663d
WalukaUI <cwaluka@yahoo.com>
2022-05-24 16:02:46 -0500

    map scled up to 100vh

--- src/Components/Locations/Map.js
+++ src/Components/Locations/Map.js
@@ -1,40 +1,47 @@
-import React from 'react'
-import { GoogleMap, useJsApiLoader, Marker, InfoWindow } from '@react-google-maps/api';
+import React from "react";
+import {
+  GoogleMap,
+  useJsApiLoader,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
 
 const containerStyle = {
   width: "100%",
-  height: "600px"
+  height: "100vh",
 };
 
-
-function MyComponent({locations, selectedPlace, setSelectedPlace}) {
-  const [map, setMap] = React.useState(null)
+function MyComponent({ locations, selectedPlace, setSelectedPlace }) {
+  const [map, setMap] = React.useState(null);
   const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: `${process.env.REACT_APP_GOOGLE_KEY}`
-  })
-console.log(map);
+    id: "google-map-script",
+    googleMapsApiKey: `${process.env.REACT_APP_GOOGLE_KEY}`,
+  });
+  console.log(map);
   const onLoad = React.useCallback(function callback(map) {
     const bounds = new window.google.maps.LatLngBounds();
     map.fitBounds(bounds);
-    setMap(map)
-  }, [])
+    setMap(map);
+  }, []);
 
   const onUnmount = React.useCallback(function callback(map) {
-    setMap(null)
-  }, [])
+    setMap(null);
+  }, []);
 
   return isLoaded ? (
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        defaultCenter={ !selectedPlace ? {lat: 38.63217176910362, lng: -90.19383204054196}:
-        {lat: selectedPlace.latitude, lng: selectedPlace.longitude} }
-        zoom={selectedPlace?11:10}
-        onLoad={onLoad}
-        onUnmount={onUnmount}
-      >
-        { /* Child components, such as markers, info windows, etc. */ }
-        <>
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      defaultCenter={
+        !selectedPlace
+          ? { lat: 38.63217176910362, lng: -90.19383204054196 }
+          : { lat: selectedPlace.latitude, lng: selectedPlace.longitude }
+      }
+      zoom={selectedPlace ? 11 : 10}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      {/* Child components, such as markers, info windows, etc. */}
+      <>
         {locations?.map((card) => (
           <Marker
             key={card.id}
@@ -44,25 +51,27 @@ console.log(map);
               url: "/hospital logo.png",
               scaledSize: new window.google.maps.Size(25, 25),
             }}
-           
-          />))}
-          {selectedPlace && (
-                    <InfoWindow
-                      position={{
-                        lat: selectedPlace.latitude,
-                        lng: selectedPlace.longitude,
-                      }}
-                      onCloseClick={() => setSelectedPlace(null)}
-                    >
-                      <div style={{height: "70px", paddingTop: "20px"}}>
-                        <h6>{selectedPlace.name}</h6>
-                        <p>Clinic</p>
-                      </div>
-                    </InfoWindow>
-                  )}
-        </>
-      </GoogleMap>
-  ) : <></>
+          />
+        ))}
+        {selectedPlace && (
+          <InfoWindow
+            position={{
+              lat: selectedPlace.latitude,
+              lng: selectedPlace.longitude,
+            }}
+            onCloseClick={() => setSelectedPlace(null)}
+          >
+            <div style={{ height: "70px", paddingTop: "20px" }}>
+              <h6>{selectedPlace.name}</h6>
+              <p>Clinic</p>
+            </div>
+          </InfoWindow>
+        )}
+      </>
+    </GoogleMap>
+  ) : (
+    <></>
+  );
 }
 
-export default React.memo(MyComponent)
+export default React.memo(MyComponent);

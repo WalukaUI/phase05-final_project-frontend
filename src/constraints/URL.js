00053a4239a5fd87ca8326515b21fcb79c66ebac
WalukaUI <cwaluka@yahoo.com>
2023-03-11 01:08:04 -0600

    constrain backend ur changed

--- src/constraints/URL.js
+++ src/constraints/URL.js
@@ -2,6 +2,9 @@
 const BASE_URL=
 process.env.NODE_ENV === "development"
 ?"http://localhost:3000"
-:"https://guarded-hamlet-25175.herokuapp.com"
+:"https://doc-appointment-booking-app.onrender.com"
 
 export default BASE_URL
+
+
+//"https://guarded-hamlet-25175.herokuapp.com"

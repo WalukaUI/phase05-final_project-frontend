85768fe7a850819774ab5422a879898b5059bff7
WalukaUI <cwaluka@yahoo.com>
2023-08-31 20:34:20 -0500

    API url changed

--- src/constraints/URL.js
+++ src/constraints/URL.js
@@ -2,9 +2,10 @@
 const BASE_URL=
 process.env.NODE_ENV === "development"
 ?"http://localhost:3000"
-:"https://doc-appointment-booking-app.onrender.com"
+:"https://doc-appointment-booking-app-8owr.onrender.com"
 
 export default BASE_URL
 
 
 //"https://guarded-hamlet-25175.herokuapp.com"
+//https://doc-appointment-booking-app.onrender.com

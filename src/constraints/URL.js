f74abb989b63ba29982024ca58d10cd85dd5a229
WalukaUI <cwaluka@yahoo.com>
2023-08-31 21:47:39 -0500

    render API url changed

--- src/constraints/URL.js
+++ src/constraints/URL.js
@@ -2,7 +2,7 @@
 const BASE_URL=
 process.env.NODE_ENV === "development"
 ?"http://localhost:3000"
-:"https://doc-appointment-booking-app-8owr.onrender.com"
+:"https://cnc-and-docapp-webservice.onrender.com"
 
 export default BASE_URL
 
